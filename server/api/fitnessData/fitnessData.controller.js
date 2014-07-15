49da953e5348e0e03f44726452cec7ce896c9cfc
celineotter <celine.otter@gmail.com>
2014-07-14 23:13:03 -0700

    Add new server route and server module

---
+++ server/api/fitnessData/fitnessData.controller.js
@@ -0,0 +1,57 @@
+
+
+
+exports.getWeight = function(req, res) {
+  // fitnessData.find(function (err, things) {
+  //   if(err) { return handleError(res, err); }
+  //   return res.json(200, things);
+  // });
+};
+
+
+exports.updateWeight = function(req, res) {
+};
+
+
+
+
+exports.getWeight = function(req, res) {
+};
+
+exports.updateWeight = function(req, res) {
+};
+
+
+exports.getBF = function(req, res) {
+};
+
+exports.updateBF = function(req, res) {
+};
+
+
+exports.getProtein = function(req, res) {
+};
+
+exports.updateProtein = function(req, res) {
+};
+
+
+exports.getCarbs = function(req, res) {
+};
+
+exports.updateCarbs = function(req, res) {
+};
+
+
+exports.getFat = function(req, res) {
+};
+
+exports.updateFat = function(req, res) {
+};
+
+
+exports.getCalories = function(req, res) {
+};
+
+exports.updateCalories = function(req, res) {
+};
