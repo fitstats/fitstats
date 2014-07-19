3edfdd201ea2b695ff86d68b056cef76afdaa174
Andrew Zey <andrew@andrewzey.com>
2014-07-18 21:32:39 -0700

    Deploy v0.0.8

--- server/api/fitnessData/fitnessData.model.js
+++ server/api/fitnessData/fitnessData.model.js
@@ -1,12 +1,25 @@
-// 'use strict';
-//
-// var mongoose = require('mongoose'),
-//     Schema = mongoose.Schema;
-//
-// var DailyDataSchema = new Schema({
-//   'date': ;
-//   'data': ;
-//   'user_id': Schema.ObjectId
-// });
-//
-// module.exports = mongoose.model('UserData', DailyDataSchema);
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var FitnessDataSchema = new Schema({
+  'date': String,
+  'weight': Number,
+  'bf': Number,
+  'hr': Number,
+  'bps': Number,
+  'bpd': Number,
+  'calories': Number,
+  'protein': Number,
+  'carbs': Number,
+  'fat': Number
+});
+
+FitnessDataSchema
+	.pre('save', function(next) {
+		next();
+	})
+
+module.exports = mongoose.model('FitnessData', FitnessDataSchema);
+
