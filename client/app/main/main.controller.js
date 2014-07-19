f3a273899d0f5d35e0007a54e8d87ffdc10bf2be
Andrew Zey <andrew@andrewzey.com>
2014-07-19 01:14:39 -0700

    Refactor Google Chart code for clarity

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,108 +3,48 @@
 angular.module('fitStatsApp')
   .controller('MainCtrl', function ($scope) {
     $scope.chartObject = {
-      "type": "LineChart",
-      "displayed": true,
-      "data": {
-        "cols": [
+      'type': 'LineChart',
+      'displayed': true,
+      'data': {
+        'cols': [
           {
-            "id": "month",
-            "label": "Month",
-            "type": "string",
-            "p": {}
+            'id': 'month',
+            'label': 'Month',
+            'type': 'string',
+            'p': {}
           },
           {
-            "id": "laptop-id",
-            "label": "Weight",
-            "type": "number",
-            "p": {}
+            'id': 'weight-id',
+            'label': 'Weight',
+            'type': 'number',
+            'p': {}
           }
         ],
-        "rows": [
-          {
-            "c": [
-              {
-                "v": "7-13"
-              },
-              {
-                "v": 175.0,
-                "f": "175.0 lbs"
-              }
-            ]
-          },
-          {
-            "c": [
-              {
-                "v": "7-14"
-              },
-              {
-                "v":175.2,
-                "f": "175.2 lbs"
-              }
-            ]
-          },
-          {
-            "c": [
-              {
-                "v": "7-15"
-              },
-              {
-                "v": 174.6,
-                "f": "174.6 lbs"
-              }
-            ]
-          },
-          {
-            "c": [
-              {
-                "v": "7-16"
-              },
-              {
-                "v": 174.2,
-                "f": "174.2 lbs"
-              }
-            ]
-          },
-          {
-            "c": [
-              {
-                "v": "7-17"
-              },
-              {
-                "v": 174.6,
-                "f": "174.6 lbs"
-              }
-            ]
-          },
-          {
-            "c": [
-              {
-                "v": "7-18"
-              },
-              {
-                "v": 173.8,
-                "f": "173.8 lbs"
-              }
-            ]
-          }
+        'rows': [
+          {'c': [ {'v': '7-13'}, {'v': 175.0, 'f': '175.0 lbs'} ] },
+          {'c': [ {'v': '7-14'}, {'v': 175.2, 'f': '175.2 lbs'} ] },
+          {'c': [ {'v': '7-15'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
+          {'c': [ {'v': '7-16'}, {'v': 174.2, 'f': '174.2 lbs'} ] },
+          {'c': [ {'v': '7-17'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
+          {'c': [ {'v': '7-18'}, {'v': 173.8, 'f': '173.8 lbs'} ] }
         ]
       },
-      "options": {
-        "title": "Weight Loss Over Time",
-        "isStacked": "true",
-        "fill": 20,
-        "displayExactValues": true,
-        "vAxis": {
-          // "title": "Weight",
-          "gridlines": {
-            "count": 10
+      'options': {
+        'title': 'Weight Loss Over Time',
+        'isStacked': 'true',
+        'fill': 20,
+        'displayExactValues': true,
+        'vAxis': {
+          // 'title': 'Weight',
+          'gridlines': {
+            'count': 10
           }
         },
-        "hAxis": {
-          // "title": "Date"
+        'hAxis': {
+          // 'title': 'Date'
         }
       },
-      "formatters": {},
-      "view": {}
-    }
+      'formatters': {},
+      'view': {}
+    };
   });
