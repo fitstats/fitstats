3a4d83e7ad8eefb53dd0d228ad088469149de751
Andrew Zey <andrew@andrewzey.com>
2014-07-21 21:25:34 -0700

    (feat) Add remaning charts to 'stats' route with dummy data

--- client/app/stats/stats.controller.js
+++ client/app/stats/stats.controller.js
@@ -29,11 +29,10 @@ angular.module('fitStatsApp')
       },
       'bf': {
         chartTitle: 'Body Fat % Change Over Time',
-        chartLabel: 'Body Fat (%)',
         chartData: {
           cols: [
             { id: 'month', label: 'Month', type: 'string', p: {} },
-            { id: 'field-id', label: 'Weight (lbs)', type: 'number', p: {} }
+            { id: 'field-id', label: 'Body Fat (%)', type: 'number', p: {} }
           ],
           rows: [
             {'c': [ {'v': '7-13'}, {'v': 13.2, 'f': '13.2 %'} ] },
@@ -47,11 +46,10 @@ angular.module('fitStatsApp')
       },
       'hr': {
         chartTitle: 'Resting Heart Rate Change Over Time',
-        chartLabel: 'Heart Rate (BPM)',
         chartData: {
           cols: [
             { id: 'month', label: 'Month', type: 'string', p: {} },
-            { id: 'field-id', label: 'Weight (lbs)', type: 'number', p: {} }
+            { id: 'field-id', label: 'Heart Rate (BPM)', type: 'number', p: {} }
           ],
           rows: [
             {'c': [ {'v': '7-13'}, {'v': 75, 'f': '75 bpm'} ] },
@@ -65,59 +63,90 @@ angular.module('fitStatsApp')
       },
       'bp': {
         chartTitle: 'Blood Pressure Change Over Time',
-        chartLabel: 'Blood Pressure (mmHg)',
-        chartData: [  {'c': [ {'v': '7-13'}, {'v': 175.0, 'f': '175.0 lbs'} ] },
-                      {'c': [ {'v': '7-14'}, {'v': 175.2, 'f': '175.2 lbs'} ] },
-                      {'c': [ {'v': '7-15'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-16'}, {'v': 174.2, 'f': '174.2 lbs'} ] },
-                      {'c': [ {'v': '7-17'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-18'}, {'v': 173.8, 'f': '173.8 lbs'} ] }
-                    ],
+        chartData: {
+          cols: [
+            { id: 'month', label: 'Month', type: 'string', p: {} },
+            { id: 'field-id', label: 'Systolic (mmHg)', type: 'number', p: {} },
+            { id: 'field-id', label: 'Diastolic (mmHg)', type: 'number', p: {} }
+          ],
+          rows: [
+            {'c': [ {'v': '7-13'}, {'v': 130, 'f': '130 mmHg'}, {'v': 85, 'f': '85 mmHg'} ] },
+            {'c': [ {'v': '7-14'}, {'v': 125, 'f': '125 mmHg'}, {'v': 75, 'f': '85 mmHg'} ] },
+            {'c': [ {'v': '7-15'}, {'v': 125, 'f': '125 mmHg'}, {'v': 80, 'f': '85 mmHg'} ] },
+            {'c': [ {'v': '7-16'}, {'v': 130, 'f': '130 mmHg'}, {'v': 85, 'f': '85 mmHg'} ] },
+            {'c': [ {'v': '7-17'}, {'v': 125, 'f': '125 mmHg'}, {'v': 75, 'f': '85 mmHg'} ] },
+            {'c': [ {'v': '7-18'}, {'v': 120, 'f': '120 mmHg'}, {'v': 70, 'f': '85 mmHg'} ] }
+          ]
+        }
       },
       'calories': {
         chartTitle: 'Calories Consumed Over Time',
         chartLabel: 'Calories (kcal)',
-        chartData: [  {'c': [ {'v': '7-13'}, {'v': 175.0, 'f': '175.0 lbs'} ] },
-                      {'c': [ {'v': '7-14'}, {'v': 175.2, 'f': '175.2 lbs'} ] },
-                      {'c': [ {'v': '7-15'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-16'}, {'v': 174.2, 'f': '174.2 lbs'} ] },
-                      {'c': [ {'v': '7-17'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-18'}, {'v': 173.8, 'f': '173.8 lbs'} ] }
-                    ],
-
+        chartData: {
+          cols: [
+            { id: 'month', label: 'Month', type: 'string', p: {} },
+            { id: 'field-id', label: 'Calories (kcal)', type: 'number', p: {} }
+          ],
+          rows: [
+            {'c': [ {'v': '7-13'}, {'v': 2400, 'f': '2,400 kcal'} ] },
+            {'c': [ {'v': '7-14'}, {'v': 2350, 'f': '2,350 kcal'} ] },
+            {'c': [ {'v': '7-15'}, {'v': 2450, 'f': '2,450 kcal'} ] },
+            {'c': [ {'v': '7-16'}, {'v': 2000, 'f': '2,000 kcal'} ] },
+            {'c': [ {'v': '7-17'}, {'v': 2700, 'f': '2,700 kcal'} ] },
+            {'c': [ {'v': '7-18'}, {'v': 2300, 'f': '2,300 kcal'} ] }
+          ]
+        }
       },
       'protein': {
         chartTitle: 'Protein Consumed Over Time',
-        chartLabel: 'Protein (g)',
-        chartData: [  {'c': [ {'v': '7-13'}, {'v': 175.0, 'f': '175.0 lbs'} ] },
-                      {'c': [ {'v': '7-14'}, {'v': 175.2, 'f': '175.2 lbs'} ] },
-                      {'c': [ {'v': '7-15'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-16'}, {'v': 174.2, 'f': '174.2 lbs'} ] },
-                      {'c': [ {'v': '7-17'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-18'}, {'v': 173.8, 'f': '173.8 lbs'} ] }
-                    ],
+        chartData: {
+          cols: [
+            { id: 'month', label: 'Month', type: 'string', p: {} },
+            { id: 'field-id', label: 'Protein (g)', type: 'number', p: {} }
+          ],
+          rows: [
+            {'c': [ {'v': '7-13'}, {'v': 160, 'f': '160 grams'} ] },
+            {'c': [ {'v': '7-14'}, {'v': 150, 'f': '150 grams'} ] },
+            {'c': [ {'v': '7-15'}, {'v': 155, 'f': '155 grams'} ] },
+            {'c': [ {'v': '7-16'}, {'v': 140, 'f': '140 grams'} ] },
+            {'c': [ {'v': '7-17'}, {'v': 158, 'f': '158 grams'} ] },
+            {'c': [ {'v': '7-18'}, {'v': 165, 'f': '165 grams'} ] }
+          ]
+        }
       },
       'carbs': {
         chartTitle: 'Carbs Consumed Over Time',
-        chartLabel: 'Carbs (g)',
-        chartData: [  {'c': [ {'v': '7-13'}, {'v': 175.0, 'f': '175.0 lbs'} ] },
-                      {'c': [ {'v': '7-14'}, {'v': 175.2, 'f': '175.2 lbs'} ] },
-                      {'c': [ {'v': '7-15'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-16'}, {'v': 174.2, 'f': '174.2 lbs'} ] },
-                      {'c': [ {'v': '7-17'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-18'}, {'v': 173.8, 'f': '173.8 lbs'} ] }
-                    ],
+        chartData: {
+          cols: [
+            { id: 'month', label: 'Month', type: 'string', p: {} },
+            { id: 'field-id', label: 'Carbs (g)', type: 'number', p: {} }
+          ],
+          rows: [
+            {'c': [ {'v': '7-13'}, {'v': 160, 'f': '160 grams'} ] },
+            {'c': [ {'v': '7-14'}, {'v': 150, 'f': '150 grams'} ] },
+            {'c': [ {'v': '7-15'}, {'v': 155, 'f': '155 grams'} ] },
+            {'c': [ {'v': '7-16'}, {'v': 140, 'f': '140 grams'} ] },
+            {'c': [ {'v': '7-17'}, {'v': 158, 'f': '158 grams'} ] },
+            {'c': [ {'v': '7-18'}, {'v': 165, 'f': '165 grams'} ] }
+          ]
+        }
       },
       'fat': {
         chartTitle: 'Fat Consumed Over Time',
-        chartLabel: 'Fat (g)',
-        chartData: [  {'c': [ {'v': '7-13'}, {'v': 175.0, 'f': '175.0 lbs'} ] },
-                      {'c': [ {'v': '7-14'}, {'v': 175.2, 'f': '175.2 lbs'} ] },
-                      {'c': [ {'v': '7-15'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-16'}, {'v': 174.2, 'f': '174.2 lbs'} ] },
-                      {'c': [ {'v': '7-17'}, {'v': 174.6, 'f': '174.6 lbs'} ] },
-                      {'c': [ {'v': '7-18'}, {'v': 173.8, 'f': '173.8 lbs'} ] }
-                    ],
+        chartData: {
+          cols: [
+            { id: 'month', label: 'Month', type: 'string', p: {} },
+            { id: 'field-id', label: 'Fat (g)', type: 'number', p: {} }
+          ],
+          rows: [
+            {'c': [ {'v': '7-13'}, {'v': 160, 'f': '160 grams'} ] },
+            {'c': [ {'v': '7-14'}, {'v': 150, 'f': '150 grams'} ] },
+            {'c': [ {'v': '7-15'}, {'v': 155, 'f': '155 grams'} ] },
+            {'c': [ {'v': '7-16'}, {'v': 140, 'f': '140 grams'} ] },
+            {'c': [ {'v': '7-17'}, {'v': 158, 'f': '158 grams'} ] },
+            {'c': [ {'v': '7-18'}, {'v': 165, 'f': '165 grams'} ] }
+          ]
+        }
       }
     };
 
